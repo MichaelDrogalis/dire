09dc0512b2e8dde2ae8e96f5318105d190053637
Michael Drogalis <mjd3089@rit.edu>
2012-12-29 13:56:50 -0500

    Let's use Clojure 1.5.0.

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url "https://github.com/MichaelDrogalis/dire"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.0-RC1"]])
