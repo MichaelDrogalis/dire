9ed449f7065a75210edc2acdadac1793c9a88ee9
Michael Drogalis <mjd3089@rit.edu>
2012-12-29 15:32:28 -0500

    Tests for default exception.

--- test/dire/test/core_test.clj
+++ test/dire/test/core_test.clj
@@ -13,6 +13,12 @@
   java.lang.NullPointerException
   (fn [e & args] :npe-handler))
 
+(fact (supervise divider 10 2) => 5)
 (fact (supervise divider 10 0) => :division-by-zero-handler)
 (fact (supervise divider 10 nil) => :npe-handler)
 
+(defn unhandled-divider [a b]
+  (/ a b))
+
+(fact (with-out-str (supervise unhandled-divider 5 0))
+      => "Untrapped exception: #<ArithmeticException java.lang.ArithmeticException: Divide by zero>\n")
