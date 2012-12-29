386df6ab2ff31049df059e76e3a37c23346a8ec4
Michael Drogalis <mjd3089@rit.edu>
2012-12-29 15:23:31 -0500

    Added Midje dependencies.

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "https://github.com/MichaelDrogalis/dire"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0-RC1"]])
+  :repositories {"stuart" "http://stuartsierra.com/maven2"}  
+  :dependencies [[org.clojure/clojure "1.5.0-RC1"]
+                 [midje "1.4.0"]
+                 [com.stuartsierra/lazytest "1.2.3"]]
+  :plugins [[lein-midje "2.0.0-SNAPSHOT"]])
