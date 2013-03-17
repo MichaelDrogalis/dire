f88ffbfb32dc2a9cb03808c5ee095ff6228292b4
Michael Drogalis <mjd3089@rit.edu>
2013-03-17 06:50:30 -0400

    Fixing another bug related to multiple preconditions.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject dire "0.3.0"
+(defproject dire "0.4.0-SNAPSHOT"
   :description "Erlang-style supervisor error handling for Clojure"
   :url "https://github.com/MichaelDrogalis/dire"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"stuart" "http://stuartsierra.com/maven2"}  
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.nrepl "0.2.0-RC1"]
                  [midje "1.4.0"]
                  [com.stuartsierra/lazytest "1.2.3"]
--- test/dire/test/multiple_hook_test.clj
+++ test/dire/test/multiple_hook_test.clj
@@ -12,4 +12,25 @@
   (fn [_ _]))
 
 (fact (task-a true) => true)
+(fact (task-b true true) => true)
+
+(with-precondition! #'task-a
+  :not-2
+  (fn [x] (not= x 2)))
+
+(with-handler! #'task-a
+  {:precondition :not-2}
+  (fn [e x] "Was 2"))
+
+(with-precondition! #'task-a
+  :not-4
+  (fn [x] (not= 4 x)))
+
+(with-handler! #'task-a
+  {:precondition :not-4}
+  (fn [e x] "Was 4"))
+
+(fact (task-a 2) => "Was 2")
+(fact (task-a 4) => "Was 4")
+(fact (task-a 1) => true)
 
