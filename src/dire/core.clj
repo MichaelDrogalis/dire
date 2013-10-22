9ab3c8230efa4ac1a20378b0833c834ab09258f8
Dylan Paris <dylan.paris+github@gmail.com>
2013-10-22 12:48:19 -0700

    Add support for slingshot selector syntax when using with-handler

--- src/dire/core.clj
+++ src/dire/core.clj
@@ -4,11 +4,11 @@
 
 (defn with-handler
   "Binds handler-fn as the receiver of an exception of type
-   exception-type when task-var is invoked via supervise."
-  ([task-var docstring? exception-type handler-fn]
-     (with-handler task-var exception-type handler-fn))
-  ([task-var exception-type handler-fn]
-     (alter-meta! task-var assoc-in [:dire/error-handlers exception-type] handler-fn)))
+   exception-selector when task-var is invoked via supervise."
+  ([task-var docstring? exception-selector handler-fn]
+     (with-handler task-var exception-selector handler-fn))
+  ([task-var exception-selector handler-fn]
+     (alter-meta! task-var assoc-in [:dire/error-handlers exception-selector] handler-fn)))
 
 (defn with-finally
   "Binds finally-fn as the last piece of code to execute within
@@ -93,6 +93,55 @@
   (when-let [finally-fn (:dire/finally task-metadata)]
     (apply finally-fn args)))
 
+(defn selector-type [selector]
+  (cond
+   (class? selector)
+   :class-name
+
+   (and (vector? selector)
+        (even? (count selector)))
+   :key-values
+
+   (fn? selector)
+   :predicate))
+
+(defmulti selector-matches?
+  (fn [selector _] (selector-type selector))
+  :default :unknown-selector)
+
+(defmethod selector-matches? :class-name [selector object]
+  (instance? selector object))
+
+(defmethod selector-matches? :key-values [selector object]
+  (let [key-val-tests (for [[key val] (partition 2 selector)]
+                        (= (get object key) val))]
+    (every? true? key-val-tests)))
+
+(defmethod selector-matches? :predicate [selector object]
+  ;; NOTE - I'm not sure about this. Slingshot doesn't even guard against
+  ;;        invalid predicate application, so I don't know what the best
+  ;;        way to handle, say, an illegal argument would be. Seems sane
+  ;;        to just return false in exceptional cases, which would indicate
+  ;;        no selector match was found.  -- DP, 21-OCT-2013
+  (try
+    (selector object)
+    (catch Throwable e false)))
+
+(defmethod selector-matches? :unknown-selector [selector object]
+  ;; NOTE - For now, just return false, but might want to throw something
+  ;;        here to more closely match the slingshot behavior. I think
+  ;;        slingshot raises a new exception indicating an invalid form was
+  ;;        passed to the catch clause within the try+ call.
+  ;;        -- DP, 21-OCT-2013
+  false)
+
+(defn- match-handler-selector [handlers object]
+  (let [[[selector _]] (seq handlers)]
+    (cond
+     (empty? handlers) nil
+     (selector-matches? selector object) selector
+     :else (recur (rest handlers) object))))
+
 (defn- default-error-handler [exception & _]
   (throw exception))
 
@@ -110,9 +159,24 @@
     (post-handler conditions (:result conditions))
     (throw+ conditions)))
 
-(defmethod apply-handler :default [task-meta e args]
-  (let [handler (get (:dire/error-handlers task-meta) (type e) default-error-handler)]
-    (apply handler e args)))
+(defmethod apply-handler :default [task-meta thrown-object args]
+  (let [error-handlers (:dire/error-handlers task-meta)
+        handler        (get error-handlers
+                            (match-handler-selector error-handlers
+                                                    thrown-object)
+                            default-error-handler)]
+    (apply handler thrown-object args)))
+
+(defn- generate-catch-predicate
+  "Creates a predicate closure around the task context which returns a boolean
+   indicating whether the object tested matches any known error handler
+   selectors."
+  [task-meta]
+  (let [handlers (:dire/error-handlers task-meta)]
+    (fn [object]
+      (if (match-handler-selector handlers object)
+        true
+        false))))
 
 (defn- supervised-meta [task-meta task-var & args]
   (try+
@@ -127,8 +191,8 @@
      (apply-handler :precondition task-meta conditions args))
    (catch [:type :dire.core/postcondition] {:as conditions}
      (apply-handler :postcondition task-meta conditions args))
-   (catch Exception e
-     (apply-handler task-meta e args))
+   (catch (generate-catch-predicate task-meta) e
+       (apply-handler task-meta e args))
    (finally
     (apply eval-finally task-meta args))))
 
@@ -144,10 +208,10 @@
 
 (defn with-handler!
   "Same as with-handler, but task-var can be invoked without supervise. (e.g. (task-var args))"
-  ([task-var docstring? exception-type handler-fn]
-     (with-handler! task-var exception-type handler-fn))
-  ([task-var exception-type handler-fn]
-     (with-handler task-var exception-type handler-fn)
+  ([task-var docstring? exception-selector handler-fn]
+     (with-handler! task-var exception-selector handler-fn))
+  ([task-var exception-selector handler-fn]
+     (with-handler task-var exception-selector handler-fn)
      (hook-supervisor-to-fn task-var)))
 
 (defn with-finally!
@@ -197,4 +261,3 @@
   ([task-var f]
      (with-post-hook task-var f)
      (hook-supervisor-to-fn task-var)))
-
---
+++ test/dire/test/slingshot_handler_tests.clj
@@ -0,0 +1,43 @@
+(ns dire.test.slingshot-handler-tests
+  (:require [midje.sweet :refer :all]
+            [dire.core :refer :all]
+            [slingshot.slingshot :refer :all]))
+
+(defn test-function [arg]
+  (cond
+   (= arg "MAP") (throw+ {:type :custom-exception :message "MAP THROWN"})
+   (= arg "EXCEPTION") (throw+ (java.lang.IllegalArgumentException. "EXCEPTION THROWN"))
+   (= arg "PREDICATE") (throw+ [1 2 3])
+   :else arg))
+
+(defn test-selector-predicate [object]
+  (= (count object) 3))
+
+;; Using slingshot selectors within with-handler
+;; NOTE - Support for full selector forms is TBD  -- DP, 20-OCT-2013
+
+;; Test slingshot match against key-values vector
+(with-handler!
+  #'test-function
+  [:type :custom-exception]
+  (fn [e & args] (:message e)))
+
+;; Test slingshot match against class name
+(with-handler!
+  #'test-function
+  java.lang.IllegalArgumentException
+  (fn [e & args] (.getMessage e)))
+
+;; Test slingshot match against predicate function
+(with-handler!
+  #'test-function
+  test-selector-predicate
+  (fn [e & args] "LENGTH THREE VECTOR THROWN"))
+
+
+;; ## Slingshot Facts
+
+(fact :slingshot (test-function "DATA") => "DATA")
+(fact :slingshot (test-function "MAP") => "MAP THROWN")
+(fact :slingshot (test-function "EXCEPTION") => "EXCEPTION THROWN")
+(fact :slingshot (test-function "PREDICATE") => "LENGTH THREE VECTOR THROWN")
