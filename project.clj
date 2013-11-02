c6547fdbfd8e57716d7ec00496e2cb24213a6a9b
Dylan Paris <dylan.paris+github@gmail.com>
2013-11-01 21:52:22 -0700

    Add complementary remove-* functions to deregister dire hooks

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.incubator "0.1.3"]
                  [slingshot "0.10.3"]
                  [robert/hooke "1.3.0"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]
