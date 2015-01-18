6ec20fbdf4b1f1f8fb92cb0c1ea5fdf6326f05d9
Matthew Darling <matthew.darling@cenx.com>
2015-01-18 11:48:03 -0500

    Some easy functions for adding hooks

--- src/dire/metadata.clj
+++ src/dire/metadata.clj
@@ -3,3 +3,16 @@
   metadata of functions, and applied or removed automatically as
   needed."
   (:require [dire.core :refer :all]))
+
+;;; Implementation functions
+(defn- apply-dire-wrap-hook
+  [fn-var hook-fn]
+  (dire/with-wrap-hook! fn-var
+    (-> hook-fn resolve meta :doc)
+    hook-fn))
+
+(defn- apply-dire-eager-pre-hook
+  [fn-var hook-fn]
+  (dire/with-eager-pre-hook! fn-var
+    (-> hook-fn resolve meta :doc)
+    hook-fn))
