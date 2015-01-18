b2a1127703aff72aab368511c325e29a75c77758
Matthew Darling <matthew.darling@cenx.com>
2015-01-18 11:47:05 -0500

    New ns for specifying Dire usage via metadata

---
+++ src/dire/metadata.clj
@@ -0,0 +1,5 @@
+(ns dire.metadata
+  "Functions to allow Dire hooks and conditions to be specified in the
+  metadata of functions, and applied or removed automatically as
+  needed."
+  (:require [dire.core :refer :all]))
