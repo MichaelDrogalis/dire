cde912aa5c046e2cba93002828840cc735c3cfc8
Matthew Darling <matthew.darling@cenx.com>
2015-01-18 12:33:29 -0500

    Fix improper namespacing

--- src/dire/metadata.clj
+++ src/dire/metadata.clj
@@ -7,13 +7,13 @@
 ;;; Implementation functions
 (defn- apply-dire-wrap-hook!
   [fn-var hook-fn]
-  (dire/with-wrap-hook! fn-var
+  (with-wrap-hook! fn-var
     (-> hook-fn resolve meta :doc)
     hook-fn))
 
 (defn- apply-dire-eager-pre-hook!
   [fn-var hook-fn]
-  (dire/with-eager-pre-hook! fn-var
+  (with-eager-pre-hook! fn-var
     (-> hook-fn resolve meta :doc)
     hook-fn))
 
@@ -22,28 +22,28 @@
   (let [pre-cond-name (-> pre-cond-fn resolve meta ::pre-name)
         correct-handler (first (filter #(= pre-cond-name (-> % resolve meta ::pre-name))
                                        possible-handlers))]
-    (dire/with-precondition! fn-var
+    (with-precondition! fn-var
       (-> pre-cond-fn resolve meta :doc)
       pre-cond-name
       pre-cond-fn)
-    (dire/with-handler! fn-var
+    (with-handler! fn-var
       (-> correct-handler resolve meta :doc)
       {:precondition pre-cond-name}
       correct-handler)))
 
 (defn- remove-dire-wrap-hook!
   [fn-var hook-fn]
-  (dire/remove-wrap-hook! fn-var hook-fn))
+  (remove-wrap-hook! fn-var hook-fn))
 
 (defn- remove-dire-eager-pre-hook!
   [fn-var hook-fn]
-  (dire/remove-eager-pre-hook! fn-var hook-fn))
+  (remove-eager-pre-hook! fn-var hook-fn))
 
 (defn- remove-dire-pre!
   [fn-var pre-cond-fn]
   (let [pre-cond-name (-> pre-cond-fn resolve meta ::pre-name)]
-    (dire/remove-precondition! fn-var pre-cond-name)
-    (dire/remove-handler! fn-var pre-cond-name)))
+    (remove-precondition! fn-var pre-cond-name)
+    (remove-handler! fn-var pre-cond-name)))
 
 ;;; Public API
 (defn apply-dire-meta!
